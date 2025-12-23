be02ef08d7463fd26b1fb8b2e4264a7f673382e5
Eyad Bustami <102246457+ebustami@users.noreply.github.com>
2025-12-23 11:31:21 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
  variable "instance_name" {
   description = "Value of the EC2 instance's Name tag."
   type        = string
-  default     = "learn-terraform"
+# default     = "learn-terraform"
+  default     = "learn-hcp-terraform-upstream"
 }
 
 variable "instance_type" {
   description = "The EC2 instance's type."
   type        = string
   default     = "t2.micro"
+
 }
